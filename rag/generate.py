55105106f1affe08debf3dfd1d5a20ab535a22a9
RAJIV KUMAR <55841096+redmas45@users.noreply.github.com>
2026-04-09 05:59:45 +0530

    model name change

--- rag/generate.py
+++ rag/generate.py
@@ -58,7 +58,7 @@ INSTRUCTIONS:
                 "Content-Type": "application/json"
             },
             json={
-                "model": "llama3-8b-8192",
+                "model": "model": "llama-3.3-70b-versatile",
                 "messages": [
                     {"role": "user", "content": prompt}
                 ],
